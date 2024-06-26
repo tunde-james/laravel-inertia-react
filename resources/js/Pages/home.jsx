94b58cbd992f908aa0ba17b2165335256ab8bd75
Tunde James <tundejames04@gmail.com>
2024-06-26 01:21:45 +0100

    Preserve scroll position

--- resources/js/Pages/home.jsx
+++ resources/js/Pages/home.jsx
@@ -1,9 +1,14 @@
+import { Link } from "@inertiajs/react";
 import React from "react";
 
 export default function Home() {
     return (
         <>
             <h1 className="title">Hello User</h1>
+
+            <Link preserveScroll href="/" className="block title mt-[1000px]">
+                {new Date().toLocaleTimeString()}
+            </Link>
         </>
     );
 }
