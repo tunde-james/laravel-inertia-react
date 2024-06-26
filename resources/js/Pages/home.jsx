046702c4f3acf1263a6bc2f44fce7347cc37add6
Tunde James <tundejames04@gmail.com>
2024-06-26 11:34:38 +0100

    Listing resources with pagination.

--- resources/js/Pages/home.jsx
+++ resources/js/Pages/home.jsx
@@ -9,7 +9,7 @@ export default function Home({ posts }) {
             <h1 className="title">Hello</h1>
 
             <div>
-                {posts.map((post) => (
+                {posts.data.map((post) => (
                     <div key={post.id} className="p-4 border-b">
                         <div className="text-sm text-slate-600">
                             <span>Posted on: </span>
@@ -20,6 +20,27 @@ export default function Home({ posts }) {
                         <p className="font-medium">{post.body}</p>
                     </div>
                 ))}
+            </div>
+
+            <div className="py-12 px-4">
+                {posts.links.map((link) =>
+                    link.url ? (
+                        <Link
+                            key={link.label}
+                            href={link.url}
+                            dangerouslySetInnerHTML={{ __html: link.label }}
+                            className={`p-1 mx-1 ${
+                                link.active ? "text-blue-500 font-bold" : ""
+                            }`}
+                        />
+                    ) : (
+                        <span
+                            key={link.label}
+                            dangerouslySetInnerHTML={{ __html: link.label }}
+                            className="p-1 mx-1 text-slate-300"
+                        ></span>
+                    )
+                )}
             </div>
         </>
     );
