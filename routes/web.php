115c8320b24ea13e0052969ba8e24721a548d259
Tunde James <tundejames04@gmail.com>
2024-06-26 10:51:24 +0100

    Create posts resource controller and model.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    sleep(2);
-    return Inertia::render('home', ['name' => 'Mike']);
-});
+Route::get('/', [PostController::class, 'index']);
+
+Route::resource('posts', PostController::class)->except('index');
